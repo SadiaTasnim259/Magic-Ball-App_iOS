89b488c9b1cde67f8b5198b37a69c3306bbdb781
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-06-23 22:01:55 +0600

    magic app using random image from image array

--- Magic Ball App/ViewController.swift
+++ Magic Ball App/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image =  ["Bakugo","Ichigo","Koro_Sensei","Luffy","Midoriya","Naruto","Sanji","Shoto","Tanjiro","Zoro"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func seeButtonPressed(_ sender: UIButton) {
+        
+        var randomImage = image.randomElement() ?? ""
+        
+        var showImage = UIImage(named: randomImage)
+        imageView.image = showImage
+        
+    }
 }
 
